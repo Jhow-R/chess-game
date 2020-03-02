f277aeefde899496ab063c875ecdc8d20010558a
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-03-02 11:23:12 -0300

    Exception personalizada para erros no tabuleiro

--- Chess/Tabuleiro/Tabuleiro.cs
+++ Chess/Tabuleiro/Tabuleiro.cs
@@ -19,10 +19,39 @@ namespace Tabuleiros
             return pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao posicao)
+        {
+            return pecas[posicao.Linha, posicao.Coluna];
+        }
+
+        public bool ExistePeca(Posicao posicao)
+        {
+            ValidarPosicao(posicao);
+            return Peca(posicao) != null;
+        }
+
         public void PosicionarPeca(Peca peca, Posicao posicao)
         {
+            if (ExistePeca(posicao))
+            {
+                throw new TabuleiroException("Posição já foi ocupada por uma peça!");
+            }
             pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if (!PosicaoValida(posicao))
+                throw new TabuleiroException("Posição inválida!");
+        }
+
+        internal bool PosicaoValida(Posicao posicao)
+        {
+            if (posicao.Linha < 0 || posicao.Linha >= Linhas || posicao.Coluna >= Colunas)
+                return false;
+
+            return true;
         }
     }
 }
